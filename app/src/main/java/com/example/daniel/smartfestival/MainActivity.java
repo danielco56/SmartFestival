2945b95fa9a6516f65be5c02fcd02f3dd1a0bb4e
Cocos Daniel <danielco56@gmail.com>
2017-05-12 23:36:31 +0300

    First

--- app/src/main/java/com/example/daniel/smartfestival/MainActivity.java
+++ app/src/main/java/com/example/daniel/smartfestival/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.daniel.smartfestival;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Button login= (Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login= new Intent(MainActivity.this, First.class);
+                startActivity(login);
+            }
+        });
 
     }
 }
